6b4d4883c31e798ceb6c4d261f5f59f9e5836d39
Vadym <cni4ka007@gmail.com>
2020-03-20 12:49:23 +0100

    LAB3

---
+++ LAB3/vector.h
@@ -0,0 +1,34 @@
+#ifndef vector_h
+#define vector_h
+
+
+#include <iostream>
+
+
+namespace MyStuff{
+class vector {
+private:
+	int *tab;
+	int _size;	
+public:
+
+	vector(); //konstruktor domyslny, poniewaz korzystamy z niego w main.cpp(110) 
+	vector(int size);
+	vector(const vector &);
+	~vector();
+	int &operator[](int i) const; 
+	friend std::ostream &operator<<(std::ostream &, const vector &);
+	int size() const { return _size;} // zwracaca wymiar wektora
+	void push_back(int a);
+	void pop_back();
+	vector &operator=(const vector &);
+	friend vector operator+(const vector &, const vector &); // operator dodawania 2 wektorow
+	
+	
+	
+};
+
+}
+
+
+#endif
